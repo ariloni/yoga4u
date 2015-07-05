cde5973cf685264a7c5fa55c2b78b9cffee8c040
Ariel Kanamori <ari.kanamori@gmail.com>
2015-07-04 22:59:37 -0700

    Fixes sessions show with facebook object attribute names

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,4 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 		provider :facebook, ENV["facebook_client_id"], ENV["facebook_secret"],
 			scope: 'public_profile', 
 			info_fields: 'id,first_name,last_name, email, picture'
+			
 end
