5915a8041cc324ab97922de85658dfe07db1d9a8
Ariel Kanamori <ari.kanamori@gmail.com>
2015-07-04 21:40:22 -0700

    adds config/initializersomniauth.rb for facebook Oauth

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,6 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+		provider :developer unless Rails.env.production?
+		provider :facebook, ENV["facebook_client_id"], ENV["facebook_secret"],
+			scope: 'public_profile', 
+			info_fields: 'id,first_name,last_name, email, picture'
+end
