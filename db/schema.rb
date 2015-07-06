88ea451e9e625d19fa08b544f83d64284dd42a88
Ariel Kanamori <ari.kanamori@gmail.com>
2015-07-06 16:47:39 -0700

    Adds User model and Users controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150706232217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "image"
+    t.text     "bio"
+    t.integer  "phone"
+    t.string   "email"
+    t.string   "city"
+    t.string   "state"
+    t.boolean  "instructor"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
